cb0b28f7cee1a43ef1e5607d01e7e2b44c06af87
Peterson Senha <peterson.senha@gmail.com>
2023-08-23 22:57:59 -0300

    Alteração de Script
    Estatisticas gerais

---
+++ assets/js/sciptCleanHealth.js
@@ -0,0 +1,29 @@
+// Exemplo 2
+document.getElementById("estatisticasgerais").innerText = "Peterson Senha"
+const socket = new WebSocket("ws://petersonsenha.github.io/CleanHealth/");
+socket.onmessage = event => {
+  const data = JSON.parse(event.data);
+  const user = data.users[0];
+  document.getElementById("username").innerText = user.username;
+  document.getElementById("email").innerText = user.email;
+};
+/*
+AT
+OK
+AT+HTTPINIT
+OK
+AT+HTTPPARA="URL","http://www.example.com/"
+OK
+AT+HTTPPARA="Method","PUT"
+OK
+AT+HTTPPARA="Content-Type","application/json"
+OK
+AT+HTTPPUT="{\"name\":\"John Doe\",\"age\":30}"
+OK
+AT+HTTPREAD
+HTTP/1.1 200 OK
+Content-Type: application/json
+Content-Length: 10
+
+{"message":"Data sent successfully"}
+*/
