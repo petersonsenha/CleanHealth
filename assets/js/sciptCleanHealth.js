feb9a82564a3b334a9731cb3fa9b62fd065af460
Peterson Senha <peterson.senha@gmail.com>
2023-08-24 02:28:19 -0300

    Whatsapp
    Adicionado o número 41987613692 para o suporte de whatsapp

--- assets/js/sciptCleanHealth.js
+++ assets/js/sciptCleanHealth.js
@@ -23,6 +23,52 @@ socket.onmessage = event => {
   document.getElementById("username").innerText = user.username;
   document.getElementById("email").innerText = user.email;
 };
+
+var gateway = `ws://${window.location.hostname}/ws`;
+//document.getElementById("estatisticasgerais").innerText = gateway;
+  var websocket;
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    //document.getElementById("estatisticasgerais").innerText = "Trying to open a WebSocket connection...";
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage; // <-- add this line
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+    document.getElementById("estatisticasgerais").innerText = "Connection opened";
+  }
+
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+  function onMessage(event) {
+    var state;
+    if (event.data == "1"){
+      state = "ON";
+    }
+    else{
+      state = "OFF";
+    }
+    document.getElementById('state').innerHTML = state;
+  }
+
+  window.addEventListener('load', onLoad);
+
+  function onLoad(event) {
+    initWebSocket();
+    initButton();
+  }
+
+  function initButton() {
+    document.getElementById('button').addEventListener('click', toggle);
+  }
+
+  function toggle(){
+    websocket.send('toggle');
+  }
 /*
 AT
 OK
